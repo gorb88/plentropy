951eb9a48f0d721e6bebda769615711d9b86060d
Gordon Baker <email": null>
2017-10-11 16:34:05 -0700

    Implements session creation

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+get '/sessions/new' do
+
+  erb :'/sessions/new'
+end
+
+post '/sessions' do
+  user = User.find_by(username: params[:username])
+  if user
+    if user.password == params[:password]
+      session[:id] = user.id
+      redirect '/'
+    else
+      # wrong password error
+    end
+  else
+    # wrong user error
+  end
+  erb :'/sessions/new'
+end
