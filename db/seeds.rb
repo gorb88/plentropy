18d35fa4f6d49def5bfbfde746f4d57f38050fbe
Gordon Baker <email": null>
2017-10-12 16:29:58 -0700

    Implements seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,25 @@
 gorb = User.new(username: "gorb", email: "gorb@gorb.gorb")
 gorb.password = "gorb"
 gorb.save
+
+
+50.times do |i|
+  user = User.create(username: Faker::Cat.name.downcase, email: Faker::Internet.safe_email)
+  user.password = "password"
+  user.save
+end
+
+500.times do |i|
+  plant = Plant.create(
+    name: Faker::Pokemon.move,
+    species: Faker::Lovecraft.sentence(3, 1),
+    description: Faker::Coffee.notes,
+    living: Faker::Boolean.boolean(0.7),
+  )
+  plant.created_at = Faker::Time.between(DateTime.now - 3000, DateTime.now - 1)
+  if !plant.living
+    plant.died_at = Faker::Time.between(plant.created_at, DateTime.now)
+  end
+
+  User.all.sample.plants << plant
+end
