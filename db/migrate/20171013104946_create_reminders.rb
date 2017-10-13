89d00df2719707aba579fc22a5c3f7897d7ce96b
Gordon Baker <email": null>
2017-10-13 12:26:36 -0700

    Passes property and association tests

--- db/migrate/20171013104946_create_reminders.rb
+++ db/migrate/20171013104946_create_reminders.rb
@@ -1,8 +1,12 @@
 class CreateReminders < ActiveRecord::Migration
   def change
     create_table :reminders do |t|
-      t.string :rules, null: false
+      t.string :schedule, null: false
+      t.string :notes
+      t.string :details
       t.integer :plant_id
+
+      t.timestamps null: false
     end
   end
 end
--- spec/reminder_spec.rb
+++ spec/reminder_spec.rb
@@ -1,5 +1,41 @@
 require 'spec_helper'
 
 describe Reminder do
-  pending "add some examples to (or delete) /Users/apprentice/gorb/plentropy/Rakefile"
+
+  let!(:user){ User.create({ username: 'gorb', email: "gorb@gorb.gov"})}
+  let!(:plant){ Plant.create({ name: 'small cactus', species: "cactus", description: "Small and green", living: true})}
+  let!(:reminder){ Reminder.create( {schedule: IceCube::Schedule.new.to_yaml, notes: "mist lightly", details: "Never let plant become completely dry. Must be constantly wet"} )}
+
+  describe "properties" do
+
+    it "has schedule" do
+      expect(reminder.schedule).not_to be_nil
+    end
+
+    it "has notes" do
+      expect(reminder.notes).to eq("mist lightly")
+    end
+
+    it "has details" do
+      expect(reminder.details).to eq("Never let plant become completely dry. Must be constantly wet")
+    end
+
+  end
+
+  describe "schedule" do
+
+  end
+
+  describe "associations" do
+    it "belongs to a plant" do
+      plant.reminders << reminder
+      expect(reminder.plant).to eq(plant)
+    end
+    it "can access user" do
+      user.plants << plant
+      plant.reminders << reminder
+      expect(reminder.plant.user).to eq(user)
+    end
+  end
+
 end
